89d365888799155b9251941a6493ab17c1832d32
Vyacheslav Fedchenko <vyacheslav.fedchenko@gmail.com>
2023-07-03 21:43:55 +0300

    some unused imports deleted

--- src/main/java/com/epam/esm/dto/TagDto.java
+++ src/main/java/com/epam/esm/dto/TagDto.java
@@ -1,9 +1,6 @@
 package com.epam.esm.dto;
 
 import com.epam.esm.model.Tag;
-import com.epam.esm.util.Views;
-import com.fasterxml.jackson.annotation.JsonView;
-import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import lombok.*;
 
